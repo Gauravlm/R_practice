4da172afc5c42ac5c048978e220e6d9f38a9c1b5
gauravlm <gaurav.github@gmail.com>
2016-12-11 01:29:07 +0530

    graph done

--- Graph.R
+++ Graph.R
@@ -199,6 +199,30 @@ boxplot(mpg~ am.f * cyl.f,
 #-------------------------------------------------------------------------------
 #                           Dot plot                                           #
 #-------------------------------------------------------------------------------       
+#Dot plots:- provide a method of plotting a large number of labeled values on a simple
+#           horizontal scale.
 
+dotchart(mtcars$mpg,labels=row.names(mtcars),cex=0.7,
+         main="Gas milage for car models",
+         xlab="Mpg per gallon"
+         )
 
+dotchart(mtcars$hp,labels=row.names(mtcars),cex=0.6,
+         xlab="HP")
+
+#Dot plot grouped, sorted, and colored
+x <- mtcars[order(mtcars$mpg),]
+x$cyl <- factor(x$cyl)
+x$color[x$cyl==4] <- "red"
+x$color[x$cyl==6] <- "blue"
+x$color[x$cyl==8] <- "darkgreen"
+dotchart(x$mpg,
+         labels = row.names(x),
+         cex=.7,
+         groups = x$cyl,
+         gcolor = "black",
+         color = x$color,
+         pch=19,
+         main = "Gas Mileage for Car Models\ngrouped by cylinder",
+         xlab = "Miles Per Gallon")
 
